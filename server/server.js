1a77a06ea9ad7b51f4da252fb44c3f241d5e935f
Daniel Denton <danielryandenton@gmail.com>
2022-02-09 21:55:20 -0600

    connect front and back ends of application

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-
 // import the typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
-
 const { authMiddleware } = require('./utils/auth');
-
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -35,6 +33,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
